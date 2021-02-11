f300373bef347902038356b01e92e9636c459b8c
OyinloluB <oyinloluwab@gmail.com>
2021-02-11 20:21:55 +0100

    .

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
+import Payments from "./Payments";
+
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
@@ -15,9 +17,17 @@ class Header extends Component {
         );
       default:
         return (
-          <li>
-            <a href="/api/logout">Logout</a>
-          </li>
+          <>
+            <li>
+              <Payments />
+            </li>
+            <li style={{ margin: "0 10px" }}>
+              Credits: {this.props.auth.credits}
+            </li>
+            <li>
+              <a href="/api/logout">Logout</a>
+            </li>
+          </>
         );
     }
   }
@@ -26,7 +36,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <Link to={this.props.auth ? "/surveys" : "/"} className="left brand-logo">Emaily</Link>
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
